0401b4ca65f4ffca3de6813abf87e370e19c02ac
BlazerYoo <blazeryoo1921@gmail.com>
2024-10-14 01:12:08 +0000

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,css,js}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
-
+};
