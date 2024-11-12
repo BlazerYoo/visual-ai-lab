15ddc79d7f07616a83e3cb62094ca1f0560d13a3
nobliney <adamasprincess@gmail.com>
2024-11-12 13:37:29 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@
 module.exports = {
   content: ["./*.{html,css,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-orange': '#b53502',
+      }
+    },
     fontFamily: {
-      code: ['Courier New', 'Courier', 'monospace']
+      code: ['Courier New', 'Courier', 'monospace'],
+      sans:	['ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      serif: ['ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+      mono:	['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace']
     }
   },
   plugins: [require("daisyui")],
