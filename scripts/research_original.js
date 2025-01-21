2e11068918572d57559509b554643b6a917e3ac2
nobliney <adamasprincess@gmail.com>
2025-01-21 17:18:08 -0500

    move adding tags to app script

--- scripts/research_original.js
+++ scripts/research_original.js
@@ -1,5 +1,15 @@
 function researchFunc() {
+    // Add research tags to papers
+    const cvBadge = document.createElement("div");
+    cvBadge.innerHTML = '<div class="badge badge-outline badge-warning" id="cv">computer vision, machine learning</div>';
+    const hciBadge = document.createElement("div");
+    hciBadge.innerHTML = '<div class="badge badge-outline badge-success" id="hci">human-computer interaction, cognitive science</div>';
+    const fairBadge = document.createElement("div");
+    fairBadge.innerHTML = '<div class="badge badge-outline badge-info" id="fair">fairness, accountability, and transparency</div>';
 
+    const cvPapers = document.querySelectorAll(".cv");
+    const hciPapers = document.querySelectorAll(".hci");
+    const fairPapers = document.querySelectorAll(".fair");
     const allPapers = document.querySelectorAll(".cv, .hci, .fair");
 
     cvPapers.forEach(cvPaper => {
