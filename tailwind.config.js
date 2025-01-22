e26b3a9b027f64841db144ad740ad51de1714e3d
nobliney <adamasprincess@gmail.com>
2025-01-21 21:19:33 -0500

    update logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
       code: ['Courier New', 'Courier', 'monospace'],
       sans:	['ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
       serif: ['ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
-      mono:	['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace']
+      mono:	['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'],
+      typewriter: ['American Typewriter', 'serif']
     }
   },
   plugins: [require("daisyui")],
