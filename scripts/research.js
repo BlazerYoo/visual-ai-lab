6661959a5872cc69b840aaf1ceef19e86e547e63
BlazerYoo <blazeryoo1921@gmail.com>
2025-08-01 21:18:52 +0000

    run research script

--- scripts/research.js
+++ scripts/research.js
@@ -1,39 +1,36 @@
-function researchFunc() {
+const allPapers = document.querySelectorAll(".paper");
 
-    const allPapers = document.querySelectorAll(".paper");
+// Change style on research tag click
+const researchTabs = document.querySelectorAll(".research-filter");
+let researchTagSelected = false;
 
-    // Change style on research tag click
-    const researchTabs = document.querySelectorAll(".research-filter");
-    let researchTagSelected = false;
+researchTabs.forEach(researchTag => {
+    researchTag.addEventListener('click', (event) => {
 
-    researchTabs.forEach(researchTag => {
-        researchTag.addEventListener('click', (event) => {
+        researchTag.classList.toggle("badge-outline");
 
-            researchTag.classList.toggle("badge-outline");
+        const selectedTags = Array.from(document.querySelectorAll(".research-filter:not(.badge-outline)")).map(tag => tag.id);
 
-            const selectedTags = Array.from(document.querySelectorAll(".research-filter:not(.badge-outline)")).map(tag => tag.id);
+        // Show all papers if no tags are selected
+        if (selectedTags.length === 0) {
+            allPapers.forEach(paper => {
+                paper.classList.remove("hidden");
+            });
+        }
 
-            // Show all papers if no tags are selected
-            if (selectedTags.length === 0) {
-                allPapers.forEach(paper => {
-                    paper.classList.remove("hidden");
-                });
-            }
+        // Only show papers with selected tags
+        else {
+            // Hide all papers
+            allPapers.forEach(paper => {
+                paper.classList.add("hidden");
+            });
 
-            // Only show papers with selected tags
-            else {
-                // Hide all papers
-                allPapers.forEach(paper => {
-                    paper.classList.add("hidden");
-                });
+            const queryString = `.` + selectedTags.join(", .");
 
-                const queryString = `.` + selectedTags.join(", .");
-
-                const selectedPapers = document.querySelectorAll(queryString);
-                selectedPapers.forEach(paper => {
-                    paper.classList.remove("hidden");
-                });
-            }
-        });
+            const selectedPapers = document.querySelectorAll(queryString);
+            selectedPapers.forEach(paper => {
+                paper.classList.remove("hidden");
+            });
+        }
     });
-}
+});
